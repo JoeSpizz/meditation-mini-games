61db2cdaa8c9a2c28c2b73d9623a24fa02c05abb
Joe <spizjo01@gmail.com>
2022-09-18 08:52:45 -0600

    first commit for meditation game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { motion } from 'framer-motion';
 import './App.css';
 
 function App() {
+  function random (){ 
+    let rand = (Math.random())*1000/2
+    console.log(rand)
+    return rand}
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     <motion.h1 whileHover= {{scale: 1.5}} >Welcome to a Relaxing Series of Mini Games</motion.h1>
+     <motion.div className='box' drag></motion.div>
+     <motion.div className='box' animate={{x:[random(), random(), random(), random()], y:[50, 150, 200]}}></motion.div>
+     <motion.div className='box' animate={{rotate: [random(), random(), random()]}}></motion.div>
     </div>
   );
 }
